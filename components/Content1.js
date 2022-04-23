33f6fce8ad5a751172d2cda0ae258463ea2a5892
Avinash <avinashdixit499@gmail.com>
2022-04-23 17:35:54 +0530

    Testing Bottom Sheet
    Added Bottom Sheet from 3rd party library.
    Used it to save development time.

--- components/Content1.js
+++ components/Content1.js
@@ -1,7 +1,82 @@
 import React from "react";
+import {
+    StyleSheet, Text, View, Button, Dimensions, TouchableOpacity,
+} from 'react-native';
 
 const Content1 = () => {
-    
+    return (
+        <View style={styles.panel}>
+            <View style={{ alignItems: 'center' }}>
+                <Text style={styles.panelTitle}>Upload Photo</Text>
+                <Text style={styles.panelSubtitle}>Choose Your Profile Picture</Text>
+            </View>
+            <TouchableOpacity style={styles.panelButton}
+                // onPress={takePhotoFromCamera}
+                // onPressOut={takePhotoFromCamera}
+            >
+                <Text style={styles.panelButtonTitle}>Take Photo</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.panelButton}
+                // onPress={choosePhotoFromLibrary}
+                // onPressOut={choosePhotoFromLibrary}
+            >
+                <Text style={styles.panelButtonTitle}>Choose From Library</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.panelButton}
+                // onPress={() => bs.current.snapTo(1)}
+                onPressOut={() => bs.current.snapTo(2)}
+            >
+                <Text style={styles.panelButtonTitle}>Cancel</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    panelHeader: {
+        alignItems: 'center',
+    },
+    panelHandle: {
+        width: 40,
+        height: 8,
+        borderRadius: 4,
+        backgroundColor: '#00000040',
+        marginBottom: 10,
+    },
+    panelTitle: {
+        fontSize: 27,
+        height: 35,
+    },
+    panelSubtitle: {
+        fontSize: 14,
+        color: 'gray',
+        height: 30,
+        marginBottom: 10,
+    },
+    panelButton: {
+        padding: 13,
+        borderRadius: 10,
+        backgroundColor: '#FF6347',
+        alignItems: 'center',
+        marginVertical: 7,
+    },
+    panelButtonTitle: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    panel: {
+        padding: 20,
+        backgroundColor: '#FFFFFF',
+        paddingTop: 20,
+        // borderTopLeftRadius: 20,
+        // borderTopRightRadius: 20,
+        // shadowColor: '#000000',
+        // shadowOffset: {width: 0, height: 0},
+        // shadowRadius: 5,
+        // shadowOpacity: 0.4,
+    },
+});
 
 export default Content1;
