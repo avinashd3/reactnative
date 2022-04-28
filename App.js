a740890b40812f7a352566118cb8474cb012e41c
Avinash <avinashdixit499@gmail.com>
2022-04-28 15:57:40 +0530

    Testing bottomsheet
    Initial snap-point set to 330.
    RenderContent can switch between two designs.

--- App.js
+++ App.js
@@ -1,10 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import React from 'react';
+import BottomSheet from 'reanimated-bottom-sheet';
+import Animated from 'react-native-reanimated';
+import Content1 from './components/Content1';
 
 export default function App() {
+
+  const bs = React.createRef();
+  const [tog, setTog] = React.useState(false);
+  // var fall = Animated.Value(1);
+
+  const renderHeader = () => (
+    <View style={styles.rheader}>
+      <View style={styles.panelHeader}>
+        <View style={styles.panelHandle} />
+      </View>
+    </View>
+  );
+
+  const renderInner = () => (
+    <View style={styles.panel}>
+      <View style={{ alignItems: 'center' }}>
+        <Text style={styles.panelTitle}>Upload Photo</Text>
+        <Text style={styles.panelSubtitle}>Choose Your Profile Picture</Text>
+      </View>
+      <TouchableOpacity style={styles.panelButton}
+      // onPress={takePhotoFromCamera}
+      // onPressOut={takePhotoFromCamera}
+      >
+        <Text style={styles.panelButtonTitle}>Take Photo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.panelButton}
+      // onPress={choosePhotoFromLibrary}
+      // onPressOut={choosePhotoFromLibrary}
+      >
+        <Text style={styles.panelButtonTitle}>Choose From Library</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.panelButton}
+        // onPress={() => bs.current.snapTo(1)}
+        onPressOut={() => bs.current.snapTo(2)}
+      >
+        <Text style={styles.panelButtonTitle}>Cancel</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  const renderInner2 = () => (
+    <View style={styles.panel}>
+      <View style={{ alignItems: 'center' }}>
+        <Text style={styles.panelTitle}>Upload Photo</Text>
+        <Text style={styles.panelSubtitle}>Choose Your Profile Picture</Text>
+      </View>
+      <TouchableOpacity style={styles.panelButton}
+      // onPress={takePhotoFromCamera}
+      // onPressOut={takePhotoFromCamera}
+      >
+        <Text style={styles.panelButtonTitle}>Take Photo</Text>
+      </TouchableOpacity>
+      {/* <TouchableOpacity style={styles.panelButton}
+      // onPress={choosePhotoFromLibrary}
+      // onPressOut={choosePhotoFromLibrary}
+      >
+        <Text style={styles.panelButtonTitle}>Choose From Library</Text>
+      </TouchableOpacity> */}
+      <TouchableOpacity
+        style={styles.panelButton}
+        // onPress={() => bs.current.snapTo(1)}
+        onPressOut={() => bs.current.snapTo(2)}
+      >
+        <Text style={styles.panelButtonTitle}>Cancel</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Button title='Toggle'/> */}
+      <BottomSheet
+        ref={bs}
+        snapPoints={[450, 330, 0]}
+        renderContent={tog ? renderInner : renderInner2}
+        renderHeader={renderHeader}
+        initialSnap={1}
+        // callbackNode={fall}
+        enabledGestureInteraction={true}
+        enabledContentTapInteraction={false}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +96,62 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  panelHeader: {
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  panelHandle: {
+    width: 40,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: '#00000040',
+    marginBottom: 10,
+  },
+  panelTitle: {
+    fontSize: 27,
+    height: 35,
+  },
+  panelSubtitle: {
+    fontSize: 14,
+    color: 'gray',
+    height: 30,
+    marginBottom: 10,
+  },
+  panelButton: {
+    padding: 13,
+    borderRadius: 10,
+    backgroundColor: '#FF6347',
+    alignItems: 'center',
+    marginVertical: 7,
+  },
+  panelButtonTitle: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  panel: {
+    padding: 20,
+    backgroundColor: '#FFFFFF',
+    paddingTop: 20,
+    // borderTopLeftRadius: 20,
+    // borderTopRightRadius: 20,
+    // shadowColor: '#000000',
+    // shadowOffset: {width: 0, height: 0},
+    // shadowRadius: 5,
+    // shadowOpacity: 0.4,
+  },
+  rheader: {
+    backgroundColor: '#FFFFFF',
+    shadowColor: '#333333',
+    shadowOffset: { width: -1, height: -3 },
+    shadowRadius: 2,
+    shadowOpacity: 0.4,
+    // elevation: 5,
+    paddingTop: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
   },
 });
